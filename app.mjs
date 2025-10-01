4a5a05c5fdc383ab9bab30d53c3438376d855163
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2025-10-01 19:38:39 -0300

    Creación de esquema y modelo de superheroes

--- app.mjs
+++ app.mjs
@@ -4,3 +4,21 @@ import mongoose from "mongoose";
 mongoose.connect('mongodb+srv://Grupo-08:grupo08@cursadanodejs.ls9ii.mongodb.net/Node-js')
 .then(()=>console.log("Conexión exitosa a MongoDB"))
 .catch(error => console.error(`Error al conectar a MongoDB: ${error}`));
+
+//Esquema superheroes
+const superheroSchema = new mongoose.Schema({
+    nombreSuperHeroe: {type: String, required: true},
+    nombreReal: {type: String, required: true},
+    edad: {type: Number, min: 0},
+    planetaOrigin: {type: String, default: 'Desconocido'},
+    debilidad: String,
+    poderes: [String],
+    aliados: [String],
+    enemigos: [String],
+    createdAt: {type: Date, default: Date.now},
+    creador: String
+},{collection: 'Grupo-08'});
+
+//Implementación del esquema en un modelo
+const SuperHero = mongoose.model('SuperHero', superheroSchema);
+//El modelo permite realizar operaciones sobre los datos
